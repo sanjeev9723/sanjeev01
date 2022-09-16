8750137af7e52aa5b36c04afe405b609033b691a
sanjeev01 <sanjeev9723@gmail.com>
2022-09-16 11:28:01 +0530

    finishedn

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-// get body element reference
-// here we can get element reference in 5 ways
-// refer https://www.w3schools.com/js/js_htmldom_elements.asp
 const bodyEl = document.getElementsByTagName('body');
-
-// onchange event handler 
-// it will be triggered when ever we change the background color
 function changeBackgroundColor(event) {
     const backgroundColor = event?.target?.value;
     bodyEl[0].style.backgroundColor = backgroundColor;
