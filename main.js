043094b4c85a5e59fad7dd4ad86ec6e34ebccafe
sanjeev01 <sanjeev9723@gmail.com>
2022-09-16 11:05:01 +0530

    rectify

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
- const bodyE1= document.getElementsByTagName('body');
- function changeBackgroundcolour(event){
-    const backgroudcolour = event?.target?.value;
-    bodyE1[0].style.backgroundcolour = backgroudcolour;
- }
+// get body element reference
+// here we can get element reference in 5 ways
+// refer https://www.w3schools.com/js/js_htmldom_elements.asp
+const bodyEl = document.getElementsByTagName('body');
+
+// onchange event handler 
+// it will be triggered when ever we change the background color
+function changeBackgroundColor(event) {
+    const backgroundColor = event?.target?.value;
+    bodyEl[0].style.backgroundColor = backgroundColor;
+}
